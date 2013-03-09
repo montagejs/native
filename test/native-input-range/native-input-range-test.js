c6c5449dd875c744fde2a9834b477ee5e1451b71
François Frisch <francoisfrisch@gmail.com>
2013-03-08 16:58:52 -0800

    Remove test that involves the scroller

--- test/native-input-range/native-input-range-test.js
+++ test/native-input-range/native-input-range-test.js
@@ -3,19 +3,11 @@ var TestController = require("montage-testing/test-controller").TestController;
 
 exports.NativeInputRangeTest = Montage.create(TestController, {
 
-    scroll: {
-        value: null
-    },
-
     nativeInputRange1: {
         value: null
     },
 
     nativeInputRange: {
-        value: null
-    },
-
-    scroll_range: {
         value: null
     }
 
